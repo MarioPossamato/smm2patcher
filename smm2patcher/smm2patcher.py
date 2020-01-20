3ffde05e9dbac9477e8af52d1b958dbf77243824
Mario Possamato <51346942+MarioPossamato@users.noreply.github.com>
2020-01-19 20:36:21 -0500

    Add files via upload

--- smm2patcher/smm2patcher.py
+++ smm2patcher/smm2patcher.py
@@ -4,32 +4,32 @@ from binascii import hexlify, unhexlify
 def float_to_hex(f):
     return hex(struct.unpack('<I', struct.pack('<f', f))[0])
 
-smm2patcher = sys.argv[1]
-main_binary = sys.argv[2]
+main_binary = sys.argv[1]
+game_version = sys.argv[2]
 
 def remove_timer_check(): # Removes the annoying 'invalid time limit' check
-    global smm2patcher,main_binary
-    if main_binary == '1.0.0' or main_binary == '1.0.1':
-        with open(smm2patcher,'rb') as smm2binary:
+    global main_binary,game_version
+    if game_version == '1.0.0' or game_version == '1.0.1':
+        with open(main_binary,'rb') as smm2binary:
             smm2data = smm2binary.read()
             smm2data = bytearray(smm2data)
-            if main_binary == '1.0.0':
+            if game_version == '1.0.0':
                 smm2data[0x5AA7CF:0x5AA7D3] = b'\x1f\x20\x03\xd5'
-                with open(smm2patcher,'wb') as smm2binary:
+                with open(main_binary,'wb') as smm2binary:
                     smm2binary.write(smm2data)
                     print('Nop Instruction Successfully Patched To 0x7100adffe4 [Offset 0x5AA7CF]!')
-            if main_binary == '1.0.1':
+            if game_version == '1.0.1':
                 smm2data[0x5DF341:0x5AA7D3] = b'\x1f\x20\x03\xd5'
-                with open(smm2patcher,'wb') as smm2binary:
+                with open(main_binary,'wb') as smm2binary:
                     smm2binary.write(smm2data)
                     print('Nop Instruction Successfully Patched To 0x7100b3ce00! [Offset 0x5DF341]')
     else:
         print('Invalid Version Number!')
 
 def change_koopa_state(state_int): # Change the state of a Koopa Troopa when spun on/hit with a shell, only on 1.0.1
-    global smm2patcher,main_binary
-    if main_binary == '1.0.1':
-        with open(smm2patcher,'rb') as smm2binary:
+    global main_binary,game_version
+    if game_version == '1.0.1':
+        with open(main_binary,'rb') as smm2binary:
             smm2data = smm2binary.read()
             smm2data = bytearray(smm2data)
             if state_int == 0:
@@ -57,7 +57,7 @@ def change_koopa_state(state_int): # Change the state of a Koopa Troopa when spu
             if state_int > 10:
                 state = b'\x01\x02\x80\x52'
             smm2data[0x39DA3F:0x39DA43] = state
-            with open(smm2patcher,'wb') as smm2binary:
+            with open(main_binary,'wb') as smm2binary:
                 smm2binary.write(smm2data)
                 print(str(state) + ' written to 0x71007b031c [0x39DA3F]')
     else:
