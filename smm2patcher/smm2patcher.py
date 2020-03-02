20d0c1c2fff12c0e14b9fda73caa466bed8377b6
Mario Possamato <51346942+MarioPossamato@users.noreply.github.com>
2020-03-02 06:13:00 -0500

    Add files via upload

--- smm2patcher/smm2patcher.py
+++ smm2patcher/smm2patcher.py
@@ -15,60 +15,39 @@ def convert(s):
     return fp.contents.value
 
 main_binary = sys.argv[1]
-game_version = sys.argv[2]
 
-def remove_timer_check(): # Removes the annoying 'invalid time limit' check
-    global main_binary,game_version
-    if game_version == '1.0.0' or game_version == '1.0.1':
-        with open(main_binary,'rb') as smm2binary:
-            smm2data = smm2binary.read()
-            smm2data = bytearray(smm2data)
-            if game_version == '1.0.0':
-                smm2data[0x5AA7CF:0x5AA7D3] = b'\x1f\x20\x03\xd5'
-                with open(main_binary,'wb') as smm2binary:
-                    smm2binary.write(smm2data)
-                    print('Nop Instruction Successfully Patched To 0x7100adffe4 [Offset 0x5AA7CF]!')
-            if game_version == '1.0.1':
-                smm2data[0x5DF341:0x5AA7D3] = b'\x1f\x20\x03\xd5'
-                with open(main_binary,'wb') as smm2binary:
-                    smm2binary.write(smm2data)
-                    print('Nop Instruction Successfully Patched To 0x7100b3ce00! [Offset 0x5DF341]')
-    else:
-        print('Invalid Version Number!')
+def Change_SM3DW_Camera_Rotation(float):
+    global main_binary
+    hex = hex(struct.unpack('<I', struct.pack('<f', float))[0])
+    hex = bytes(hex)
+    hex = bytearray(hex)
+    hex.reverse()
+    hex = bytes(hex)
+    with open(main_binary,'rb') as smm2binary:
+        smm2data = smm2binary.read()
+        smm2data = bytearray(smm2data)
+        smm2data[0x2516311:0x2516315] = b'\x1f\x20\x03\xd5'
+        with open(main_binary,'wb') as smm2binary:
+            smm2binary.write(smm2data)
+            print('Done!')
+    
 
-def change_koopa_state(state_int): # Change the state of a Koopa Troopa when spun on/hit with a shell, only on 1.0.1
-    global main_binary,game_version
-    if game_version == '1.0.1':
-        with open(main_binary,'rb') as smm2binary:
-            smm2data = smm2binary.read()
-            smm2data = bytearray(smm2data)
-            if state_int == 0:
-                state = b'\x01\x00\x80\x52'
-            if state_int == 1:
-                state = b'\x21\x00\x80\x52'
-            if state_int == 2:
-                state = b'\x41\x00\x80\x52'
-            if state_int == 3:
-                state = b'\x61\x00\x80\x52'
-            if state_int == 4:
-                state = b'\x81\x00\x80\x52'
-            if state_int == 5:
-                state = b'\xA1\x00\x80\x52'
-            if state_int == 6:
-                state = b'\xC1\x00\x80\x52'
-            if state_int == 7:
-                state = b'\xE1\x00\x80\x52'
-            if state_int == 8:
-                state = b'\x01\x01\x80\x52'
-            if state_int == 9:
-                state = b'\x21\x01\x80\x52'
-            if state_int == 10:
-                state = b'\x01\x02\x80\x52'
-            if state_int > 10:
-                state = b'\x01\x02\x80\x52'
-            smm2data[0x39DA3F:0x39DA43] = state
-            with open(main_binary,'wb') as smm2binary:
-                smm2binary.write(smm2data)
-                print(str(state) + ' written to 0x71007b031c [0x39DA3F]')
-    else:
-        print('Invalid Version Number!')
+def Remove_Edit_Mode_Timer_Check(): # Removes the annoying 'invalid time limit' check for SMM2's edit mode
+    global main_binary
+    with open(main_binary,'rb') as smm2binary:
+        smm2data = smm2binary.read()
+        smm2data = bytearray(smm2data)
+        smm2data[0x14D4391:0x14D4395] = b'\x1f\x20\x03\xd5'
+        with open(main_binary,'wb') as smm2binary:
+            smm2binary.write(smm2data)
+            print('Nop Instruction Successfully Patched To 0x14D4390!')
+
+def Remove_Corrupted_Course_Check(): # Removes the annoying 'corrupted course' check for SMM2
+    global main_binary
+    with open(main_binary,'rb') as smm2binary:
+        smm2data = smm2binary.read()
+        smm2data = bytearray(smm2data)
+        smm2data[0x15924C1:0x15924C9] = b'\x20\x00\x80\xD2\xC0\x03\x5F\xD6'
+        with open(main_binary,'wb') as smm2binary:
+            smm2binary.write(smm2data)
+            print('Successfully Removed Corrupted Course Check!')
